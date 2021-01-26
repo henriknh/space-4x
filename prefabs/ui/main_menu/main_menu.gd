ee6520181b94c22ebc6f9d3608406aca11ff6b98
Henrik Nilsson Harnert <hello@henriknh.com>
2021-01-26 21:00:58 +0100

    Update main menu to align with enums changes

--- prefabs/ui/main_menu/main_menu.gd
+++ prefabs/ui/main_menu/main_menu.gd
@@ -4,22 +4,29 @@ var settings = preload('res://prefabs/ui/settings/settings_menu.tscn')
 
 func _ready():
 	MenuState.push(self)
-	$VBox/CreateGame/VBoxContainer/SliderWorldSize.max_value = Enums.world_size.keys().size() - 1
+
+	for world_size_value in Enums.world_size.values():
+		$VBox/CreateGame/VBoxContainer/OptionBtnWorldSize.add_item(Enums.world_size_label[world_size_value], world_size_value)
+	$VBox/CreateGame/VBoxContainer/OptionBtnWorldSize.selected = 0
+	_on_world_size_changed($VBox/CreateGame/VBoxContainer/OptionBtnWorldSize.selected)
+	$VBox/CreateGame/VBoxContainer/SpinBoxSeed.value = 0
+	_on_seed_changed($VBox/CreateGame/VBoxContainer/SpinBoxSeed.value)
+	
 	_update_view_state()
 	
 func _update_view_state():
 	$VBox/CreateGame/ButtonCreate.disabled = StateManager.has_save()
 	$VBox/StoredGame/ButtonLoad.disabled = not StateManager.has_save()
 	$VBox/StoredGame/ButtonDeleteSave.disabled = not StateManager.has_save()
-	_on_world_size_changed($VBox/CreateGame/VBoxContainer/SliderWorldSize.value)
-	_on_seed_changed($VBox/CreateGame/VBoxContainer/SpinBoxSeed.value)
 
 func _on_create():
 	Scene.goto_game()
 
-func _on_world_size_changed(value: int):
-	$VBox/CreateGame/VBoxContainer/LabelWorldSize.text = "World size: %s" % Enums.world_size_label[value]
-	WorldGenerator.set_world_size(value)
+func _on_world_size_changed(idx: int):
+	var selected_world_size = $VBox/CreateGame/VBoxContainer/OptionBtnWorldSize.get_item_id(idx)
+	print(idx)
+	print(selected_world_size)
+	WorldGenerator.set_world_size(selected_world_size)
 
 func _on_seed_changed(seed_value: int):
 	WorldGenerator.set_seed(seed_value)
