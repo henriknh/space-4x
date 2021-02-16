9f3b19f7d2c2b3ff47de565ff2c8ad576fffb8c2
Henrik Nilsson Harnert <hello@henriknh.com>
2021-02-16 17:07:55 +0100

    Fix travel of explorer AI ship

--- scripts/ai/ship/explorer.gd
+++ scripts/ai/ship/explorer.gd
@@ -4,7 +4,7 @@ func process(entity: Entity):
 	if entity.parent.faction != -1:
 		var adjacent_sites = Nav.get_adjacent_sites(entity.parent)
 		adjacent_sites.sort_custom(AIShipExplorerSort, "sort_explorer")
-		entity.set_entity_process(Enums.ship_states.travel, adjacent_sites[0])
+		entity.set_entity_process(Enums.ship_states.travel, adjacent_sites[0].id)
 	else:
 		entity.state = Enums.ship_states.colonize
 
