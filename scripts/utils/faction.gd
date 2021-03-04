e994b4379a19defcbed0708fb3b0fae90954eb86
Henrik Nilsson Harnert <hello@henriknh.com>
2021-03-04 17:21:28 +0100

    Add start resources to constants

--- scripts/utils/faction.gd
+++ scripts/utils/faction.gd
@@ -9,9 +9,9 @@ var friendliness = (Random.randf() * 2) - 1
 var explorer = Random.randf()
 var color = Color(0,0,0,0)
 var resources = {
-	'asteroid_rocks': 5,
-	'titanium': 0,
-	'astral_dust': 0
+	'asteroid_rocks': Consts.RESOURCE_START_ASTEROID_ROCKS,
+	'titanium': Consts.RESOURCE_START_TITANIUM,
+	'astral_dust': Consts.RESOURCE_START_ASTRAL_DUST
 }
 	
 func _init(faction: int) -> void:
