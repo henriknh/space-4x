1f3475f029cfaac4344b8e34f012ac46dc983332
Henrik Nilsson Harnert <hello@henriknh.com>
2021-03-28 13:55:22 +0200

    Change trail to use particles

--- prefabs/entities/ships/effects/trail.gd
+++ prefabs/entities/ships/effects/trail.gd
@@ -1,15 +1,22 @@
 extends Node2D
 
+onready var node_line: Line2D = $Line2D
+onready var node_particles: Particles2D = $Particles2D
+
 func set_emitting(is_emitting: bool) -> void:
-	$Line2D.set_emitting(is_emitting)
+	node_line.set_emitting(false)#is_emitting)
 	
 func is_emitting() -> bool:
-	return $Line2D.is_emitting
+	return node_line.is_emitting
 
 func set_color(color: Color) -> void:
 	var color1 = color
 	var color2 = color
 	color1.a = 0
 	color2.a = 0.2
-	$Line2D.gradient = Gradient.new()
-	$Line2D.gradient.colors = [color1, color2]
+	node_line.gradient = Gradient.new()
+	node_line.gradient.colors = [color1, color2]
+	node_particles.self_modulate = color
+
+func set_texture(texture: Texture) -> void:
+	node_particles.texture = texture
