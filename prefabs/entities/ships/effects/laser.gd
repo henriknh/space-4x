d7afa13de9c1ab1953d5778cddf64be3f226953a
Henrik Nilsson Harnert <hello@henriknh.com>
2021-03-29 00:29:13 +0200

    Lasers go sparkly sparkly

--- prefabs/entities/ships/effects/laser.gd
+++ prefabs/entities/ships/effects/laser.gd
@@ -1,10 +1,13 @@
 extends Line2D
 
 onready var timer: Timer = $Timer
+onready var particles: Particles2D = $Particles
 
 func emit(source: Vector2, target: Vector2, color: Color):
 	points = [source, target]
 	self_modulate = color
+	particles.position = target
+	particles.self_modulate = color
 	timer.start()
 
 func _on_destroy():
