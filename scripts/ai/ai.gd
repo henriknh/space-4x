1d9fa6237a5dc26f4084c4ca46ad5019d974bd43
Henrik Nilsson Harnert <hello@henriknh.com>
2021-03-31 13:04:55 +0200

    Do not process AI

--- scripts/ai/ai.gd
+++ scripts/ai/ai.gd
@@ -4,6 +4,7 @@ onready var planet = preload("res://scripts/ai/planet/planet.gd").new()
 onready var ship = preload("res://scripts/ai/ship/ship.gd").new()
 
 func process_entity(entity: Entity, delta: float):
+	return
 	match entity.entity_type:
 		Enums.entity_types.planet:
 			return planet.process(entity, delta)
