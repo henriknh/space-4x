62f252fe6ebbf31d5177f336c7db01c243785f29
Henrik Nilsson Harnert <hello@henriknh.com>
2021-04-08 19:35:43 +0200

    Miners distribute in the planet site

--- prefabs/entities/ships/ship_miner.gd
+++ prefabs/entities/ships/ship_miner.gd
@@ -8,8 +8,10 @@ var asteroid_rocks_max: int = 64
 const ANIMATION_CHARGE_MINER = 'charge_miner'
 var mining_power = 8
 
+var get_target_timer: Timer
 var miner_ready: bool = false
 var miner_timer: Timer
+var deliver_timer: Timer
 
 
 func create():
@@ -21,12 +23,22 @@ func create():
 func _ready():
 	add_to_group('Miner')
 	
+	get_target_timer = Timer.new()
+	get_target_timer.wait_time = 0.5
+	get_target_timer.connect("timeout", self, "_get_mining_target")
+	add_child(get_target_timer)
+	
 	miner_timer = Timer.new()
 	miner_timer.wait_time = 3
 	miner_timer.one_shot = true
 	miner_timer.connect("timeout", self, "_on_miner_ready")
 	add_child(miner_timer)
 	
+	deliver_timer = Timer.new()
+	deliver_timer.wait_time = 3
+	deliver_timer.one_shot = true
+	deliver_timer.connect("timeout", self, "_do_deliver")
+	add_child(deliver_timer)
 	
 	var animation = Animation.new()
 	animation.length = 1.2
@@ -52,28 +64,40 @@ func process(delta: float):
 		state = Enums.ship_states.idle
 		self.target = null
 		
+	if state != Enums.ship_states.mine and not get_target_timer.is_stopped():
+		get_target_timer.stop()
+		
 	if state == Enums.ship_states.idle and corporation_id == parent.corporation_id:
-		self.target = _get_mining_target()
-		if target:
-			state = Enums.ship_states.mine
+		_get_mining_target()
 	
 	if state == Enums.ship_states.mine and asteroid_rocks >= asteroid_rocks_max:
 		state = Enums.ship_states.deliver
 		self.target = parent
+	
+	elif state == Enums.ship_states.deliver:
 		
-	if state == Enums.ship_states.deliver and target_reached:
-		deliver()
-		state = Enums.ship_states.idle
+		if deliver_timer.is_stopped():
+			var collision
+			if node_raycast.is_colliding() and node_raycast.get_collider() == target:
+				collision = move_and_collide(Vector2.ZERO, true, true, true)
+			
+			if collision and collision.collider == self.target:
+				look_at(position - target.position)
+				deliver_timer.start()
+			else:
+				move(self.target.position)
+	elif state == Enums.ship_states.mine and not target:
+		_get_mining_target()
 	
-	elif state == Enums.ship_states.mine and target_reached:
-		rotation_degrees = target.rotation_degrees
-		if miner_ready:
-			_begin_mine()
-		elif miner_timer.is_stopped() and not node_animation.current_animation == ANIMATION_CHARGE_MINER:
-			miner_timer.start()
-		return 
-	
-	.process(delta)
+	elif state == Enums.ship_states.mine:
+		if move(self.target.position):
+			rotation_degrees = target.rotation_degrees
+			if miner_ready:
+				_begin_mine()
+			elif miner_timer.is_stopped() and not node_animation.current_animation == ANIMATION_CHARGE_MINER:
+				miner_timer.start()
+	else:
+		.process(delta)
 	
 func _on_miner_ready():
 	miner_ready = true
@@ -91,16 +115,38 @@ func _do_mine():
 	Instancer.asteroid_dust(self, false)
 	target.emit_signal("entity_changed")
 
-func deliver():
+func _do_deliver():
 	self.get_corporation().resources.asteroid_rocks += asteroid_rocks
 	asteroid_rocks = 0
+	state = Enums.ship_states.idle
+	self.target = null
 	parent.emit_signal("entity_changed")
 	
-func _get_mining_target() -> Entity:
+func _get_mining_target() -> void:
+	var target: Asteroid = null
+	var target_dist: float = INF
+	print(parent.asteroids.size())
 	for asteroid in parent.asteroids:
-		if not asteroid.is_dead() and asteroid.asteroid_rocks > 0:
-			return asteroid
-	return null
+		if asteroid.is_dead():
+			continue
+			
+		var neighbors_target = false
+		var dist_asteroid = position.distance_squared_to(asteroid.position)
+		
+		for ship in parent.ships:
+			if ship.ship_type == Enums.ship_types.miner and ship.target == asteroid:
+				if dist_asteroid > ship.position.distance_squared_to(asteroid.position):
+					neighbors_target = true
+					break
+		
+		if not neighbors_target and dist_asteroid < target_dist:
+			target = asteroid
+			target_dist = dist_asteroid
+	
+	if target:
+		self.target = target
+		state = Enums.ship_states.mine
+		get_target_timer.start()
 
 func save():
 	var save = .save()
