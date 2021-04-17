870331f24721f878fdb38e8463f8e281f1b66277
Henrik Nilsson Harnert <hello@henriknh.com>
2021-04-17 13:01:18 +0200

    AI explorer target unoccupied neighbor first

--- scripts/ai/ship/explorer.gd
+++ scripts/ai/ship/explorer.gd
@@ -4,7 +4,17 @@ func process(entity: Entity):
 	if entity.parent.corporation_id != 0:
 		var adjacent_sites = Nav.get_adjacent_sites(entity.parent)
 		adjacent_sites.sort_custom(AIShipExplorerSort, "sort_explorer")
-		entity.set_entity_process(Enums.ship_states.travel, adjacent_sites[0].id)
+		
+		var to_explore
+		for adjacent_site in adjacent_sites:
+			if adjacent_site.corporation_id == 0:
+				to_explore = adjacent_site
+				break
+		
+		if not to_explore:
+			to_explore = adjacent_sites[0]
+		
+		entity.set_entity_process(Enums.ship_states.travel, to_explore.id)
 	else:
 		entity.state = Enums.ship_states.colonize
 
