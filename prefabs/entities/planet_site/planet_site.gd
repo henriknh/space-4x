831ded94600e041c587fd259920cf076fa07ff77
Henrik Nilsson Harnert <hello@henriknh.com>
2021-05-16 11:56:20 +0200

    Set color of planet site

--- prefabs/entities/planet_site/planet_site.gd
+++ prefabs/entities/planet_site/planet_site.gd
@@ -3,11 +3,14 @@ extends Entity
 class_name PlanetSite
 
 onready var node_collision: CollisionPolygon = $Area/CollisionPolygon
+onready var node_border: CSGCombiner = $Border
+onready var node_border_material: SpatialMaterial = SpatialMaterial.new()
 onready var node_csg_polygon: CSGPolygon = $Border/CSGPolygon
 onready var node_csg_polygon_deflated: CSGPolygon = $Border/CSGPolygonDeflated
 
 var polygon = []
 var tiles = [] 
+var planet: Planet
 
 func _ready():
 	add_to_group('Persist')
@@ -15,9 +18,26 @@ func _ready():
 	
 	node_collision.polygon = polygon
 	
-	var polygon_deflated = Geometry.offset_polygon_2d(polygon, -0.5)[0]
+	node_border_material.flags_transparent = true
+	node_border_material.flags_unshaded = true
+	node_border_material.flags_do_not_receive_shadows = true
+	node_border_material.flags_disable_ambient_light = true
+	node_border_material.albedo_color = Color(1,1,1,0)
+	node_border.material_override = node_border_material
+	
+	var polygon_deflated = Geometry.offset_polygon_2d(polygon, -0.2)[0]
 	node_csg_polygon.polygon = polygon
 	node_csg_polygon_deflated.polygon = polygon_deflated
+	
+	planet.connect("entity_changed", self, "update_border_color")
+	update_border_color()
 
 func generate_site():
 	pass
+
+func update_border_color():
+	if self.planet.corporation_id > 0:
+		node_border_material.albedo_color = Enums.corporation_colors[self.planet.corporation_id]
+		node_border.visible = true
+	else:
+		node_border.visible = false
