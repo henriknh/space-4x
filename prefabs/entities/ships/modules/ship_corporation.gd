f772ce0feaf0e7a3d87df5b3d5bf5c2ff3ea79ab
Henrik Nilsson Harnert <hello@henriknh.com>
2021-05-16 09:34:50 +0200

    Ship corporation module show border

--- prefabs/entities/ships/modules/ship_corporation.gd
+++ prefabs/entities/ships/modules/ship_corporation.gd
@@ -3,12 +3,37 @@ extends Module
 class_name ModuleShipCorporation
 
 var node_mesh_material: SpatialMaterial
+var border_material: SpatialMaterial
 
 func init(_entity: Entity, params: Dictionary = {}) -> Spatial:
 	.init(_entity, params)
-
-func init(_entity: Entity) -> Spatial:
-	.init(_entity)
+	
+	border_material = SpatialMaterial.new()
+	border_material.flags_transparent = true
+	border_material.flags_unshaded = true
+	border_material.flags_do_not_receive_shadows = true
+	border_material.flags_disable_ambient_light = true
+	border_material.albedo_color = Color(1,1,1,0.02)
+	
+	var combiner = CSGCombiner.new()
+	combiner.material_override = border_material
+	
+	var outer = CSGCylinder.new()
+	outer.radius = params.radius
+	outer.height = 0.5
+	outer.sides = 32
+	outer.cast_shadow = false
+	
+	var inner = CSGCylinder.new()
+	inner.radius = params.radius - 0.5
+	inner.height = 0.5
+	inner.sides = 32
+	inner.cast_shadow = false
+	inner.operation = CSGShape.OPERATION_SUBTRACTION
+	
+	combiner.add_child(outer)
+	combiner.add_child(inner)
+	_entity.add_child(combiner)
 	
 	node_mesh_material = (self.entity.get_node("Mesh") as MeshInstance).material_override
 	
@@ -18,4 +43,6 @@ func init(_entity: Entity) -> Spatial:
 	return self
 	
 func update_corporation_color():
-	node_mesh_material.albedo_color = Enums.corporation_colors[self.entity.corporation_id]
+	var color = Enums.corporation_colors[self.entity.corporation_id]
+	node_mesh_material.albedo_color = color
+	border_material.albedo_color = color
